8b435c5259b968fe1ff5aee0f10aba9617252c82
Antonio Moscato <antonio.moscato@runelab.it>
2025-03-20 17:01:03 +0100

    feat(sidebar): add sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './features/products/products.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  // { path: 'products/:id', component: ProductDetailComponent },
+  // { path: 'userForm', component: UserProfileComponent },
+  { path: '**', component: NotFoundComponent }, // Rotta per gestire 404
+];
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isSidenavOpen: boolean = false; // Stato iniziale chiuso
+
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
+
+  closeSidenav() {
+    this.isSidenavOpen = false;
+  }
+}
