6a4f5f30ab602e597fb7605b1792853547f65fbf
Antonio Moscato <antonio.moscato@runelab.it>
2025-03-27 14:46:46 +0100

    fix(httpClient): fix httpClient call

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -17,28 +17,28 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   get<T>(endpoint: string, params?: HttpParams): Observable<T> {
-    return this.http.get<T>(`${this.baseUrl}/${endpoint}`, {
+    return this.http.get<T>(`${this.baseUrl}${endpoint}`, {
       headers: this.defaultHeaders,
       params: params,
     });
   }
 
   post<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/${endpoint}`, body, {
+    return this.http.post<T>(`${this.baseUrl}${endpoint}`, body, {
       headers: this.defaultHeaders,
     });
   }
 
   // PUT per aggiornamenti
   put<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.baseUrl}/${endpoint}`, body, {
+    return this.http.put<T>(`${this.baseUrl}${endpoint}`, body, {
       headers: this.defaultHeaders,
     });
   }
 
   // DELETE di un elemento
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.baseUrl}/${endpoint}`, {
+    return this.http.delete<T>(`${this.baseUrl}${endpoint}`, {
       headers: this.defaultHeaders,
     });
   }
