a363f83c6265abcbb8278eba5f6a0a1ca97ad7f6
Francesco <gobbi.francesco.04@gmail.com>
2025-05-30 04:22:03 +0200

    fix: correct environment import path and update tsconfig baseUrl and paths

--- src/app/core/services/img/img.service.ts
+++ src/app/core/services/img/img.service.ts
@@ -1,4 +1,4 @@
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environmentt';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
