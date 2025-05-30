068fe4fced2333d7c67b7ef01f5251d9b16dda22
Francesco <gobbi.francesco.04@gmail.com>
2025-05-30 03:59:08 +0200

    feat(not-found): implement 404 page with styled container and navigation button

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('404 - Page Not Found');
+
+  }
+  goTo() {
+    window.location.href = '/';
+  }
+}
