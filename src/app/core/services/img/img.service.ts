8f4c9cdeecc42b5f1349c9ee2876073d8a314827
Francesco <gobbi.francesco.04@gmail.com>
2025-05-30 04:42:18 +0200

    fix: correct environment import paths in API services

--- src/app/core/services/img/img.service.ts
+++ src/app/core/services/img/img.service.ts
@@ -1,4 +1,4 @@
-import { environment } from '@/env/environments/environment';
+import { environment } from '@/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
